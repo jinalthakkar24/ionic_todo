cf7e41b88f174398c9275a0ae8cd3e4d8706ce34
jinalthakkar2324 <55906161+jinalthakkar2324@users.noreply.github.com>
2020-10-11 14:23:01 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    children: [
+      {
+        path:'',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path:':uid',
+        loadChildren: () => import('./home/home-details/home-details.module').then( m => m.HomeDetailsPageModule)
+
+      }
+    ]
+    
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'demo',
+    loadChildren: () => import('./demo/demo.module').then( m => m.DemoPageModule)
+  },
+  
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TodoServiceService } from '../todo-service.service'
+import { Home } from './home.model';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+dataReturnFromService:any="";
+//userdata:any=[];
+userdata:Home[];
+homes:Home[];
+  constructor(public todoService:TodoServiceService,private alertController:AlertController) {}
+  saveData(){
+      var dataToSend = {name:"Fenil",job:"Programming"};
+      this.todoService.saveData(dataToSend).subscribe((dataReturnFromService)=>{
+          this.dataReturnFromService= JSON.stringify(dataReturnFromService);
+      })
+  }
+  getUser(){
+    this.todoService.getUser().subscribe((getdata)=>{
+      var anydata=<any>getdata;
+      this.userdata=anydata.data;
+      console.log(this.userdata);
+    })
+  }
+   async onDeleteClick(id:any){
+    const alert = await this.alertController.create({
+      header:"Confirm?",
+      message:"Are you sure want to delete?",
+      buttons:[{
+        text:'Cancel',
+        role:'Cancel'
+      },
+      {
+        text:'Delete',
+        handler:() => {
+          this.userdata=this.userdata.filter(home => {
+            return home.id !== id;
+          });
+          //this.todoService.deleteRecipe(id);
+        },
+      },
+      ],
+    });
+    await alert.present();
+  //  this.userdata=this.userdata.filter(home => {
+  //   return home.id !== id;
+  //   });
+  //   this.todoService.deleteRecipe(id);
+  //   this.getUser();
+  }
 }
